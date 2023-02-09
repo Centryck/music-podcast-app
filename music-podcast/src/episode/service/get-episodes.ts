20352a56baf752c7b5b7e694dc52ae7296ccf747
centryck <carlos.santillana.edu@gmail.com>
2023-02-09 19:36:08 +0100

    add getSingleEpisode service

--- music-podcast/src/episode/service/get-episodes.ts
+++ music-podcast/src/episode/service/get-episodes.ts
@@ -3,6 +3,7 @@ import { mapEpisodesResults } from "../mapper";
 import { Episode } from "../model";
 
 type GetEpisodesFromAPI = (podcastId: number) => Promise<Episode[]>;
+type GetSingleEpisode = (podcastId: number, episodeId: number ) => Promise<Episode[]>;
 
 export const getEpisodesFromItunes: GetEpisodesFromAPI = async (podcastId: number) => {
 	try {
@@ -14,4 +15,12 @@ export const getEpisodesFromItunes: GetEpisodesFromAPI = async (podcastId: numbe
 		console.error(err);
 		throw new Error("Error at retrieving the podcasts");
 	}
+}
+
+export const getSingleEpisode: GetSingleEpisode = async (podcastId: number, episodeId: number) => {
+	const allEpisodes = getEpisodesFromItunes(podcastId);
+
+	const selectedEpisode = (await allEpisodes).filter((filteredEpisode: Episode) => filteredEpisode.id === episodeId);
+
+	return selectedEpisode;
 }
--- music-podcast/src/episode/use-case/get-episodes.ts
+++ music-podcast/src/episode/use-case/get-episodes.ts
@@ -1,5 +1,9 @@
-import { getEpisodesFromItunes } from "../service";
+import { getEpisodesFromItunes, getSingleEpisode } from "../service";
 
 export const getAllEpisodes = (podcastId: number) => {
 	return getEpisodesFromItunes(podcastId);
 };
+
+export const getEpisodeById = (podcastId: number, episodeId: number) => {
+	return getSingleEpisode(podcastId, episodeId);
+}
